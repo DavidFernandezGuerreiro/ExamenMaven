16a8646faa7f7ae41830e0e017eeefa1debba02b
dfernandezguerreiro <dfernandezguerreiro@victoria2.danielcastelao.org>
2018-04-13 08:43:32 +0200

    Git init. Principal.

---
+++ src/main/java/com/mycompany/examenmaven/Principal.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.examenmaven;
+
+/**
+ *
+ * @author dfernandezguerreiro
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
